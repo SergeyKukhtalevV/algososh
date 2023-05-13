748652024ac56237e33e69b9fe19d7924aed91e3
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-05-13 13:50:06 +0300

    feature: string: added html and css

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,59 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import style from './string.module.css'
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {ElementStates} from "../../types/element-states";
+import {Circle} from "../ui/circle/circle";
+
+type TElement = {
+  value: string;
+  color: ElementStates
+}
 
 export const StringComponent: React.FC = () => {
+  const [loader, setLoader] = useState(false);
+  const [inputValue, setInputValue] = useState<TElement[]>([]);
+
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <Input maxLength={11} isLimitText={true}/>
+          <Button text={'Развернуть'}
+                  linkedList={'small'}
+                  isLoader={loader}
+                  disabled={!inputValue.length}
+
+          />
+        </div>
+        <ul className={style.containerResult}>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+          <li>
+            <Circle />
+          </li>
+        </ul>
+      </div>
     </SolutionLayout>
   );
 };
