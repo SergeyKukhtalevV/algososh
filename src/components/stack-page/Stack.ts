977216b1ede862c3cf3c41fea8e1a36e2ce30c38
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-05-16 22:25:39 +0300

    feature: stack-page: done

---
+++ src/components/stack-page/Stack.ts
@@ -0,0 +1,33 @@
+interface IStack<T> {
+  push: (item: T) => void;
+  pop: () => void;
+  peak: () => T | null;
+  getSize: () => number;
+  getStack: () => T[];
+  clearStack: () => void;
+}
+
+export class Stack<T> implements IStack<T> {
+  private container: T[] = [];
+
+  push = (item: T): void => {
+    this.container.push(item);
+  };
+
+  pop = (): void => {
+    if(this.getSize() > 0){
+      this.container.pop();
+    }
+  };
+
+  peak = (): T | null => {
+    return this.container.length ? this.container[this.getSize() - 1] : null;
+  };
+
+  getSize = () => this.container.length;
+
+  getStack = (): T[] => this.container;
+
+  clearStack = () => this.container = [];
+
+}
