ed96fd48418feaa9c73397379183ce1b6b05d120
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-05-16 16:37:15 +0300

    feature: stack-page: added html and css

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import style from "./stack-page.module.css";
+import {Button} from "../ui/button/button";
+import {Input} from "../ui/input/input";
+import {Circle} from "../ui/circle/circle";
+
 
 export const StackPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>("abcd");
+
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  }
   return (
     <SolutionLayout title="Стек">
-
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <Input type={"text"} maxLength={4} isLimitText={true} onChange={handleInput} value={inputValue}/>
+          <div className={style.changeButtons}>
+            <Button text={'Добавить'}/>
+            <Button text={'Удалить'}/>
+          </div>
+          <Button text={'Очистить'}/>
+        </div>
+        <ul className={style.containerResult}>
+          <li>
+            <Circle letter={inputValue} head={""}/>
+          </li>
+          <li>
+            <Circle letter={inputValue} head={"top"}/>
+          </li>
+        </ul>
+      </div>
     </SolutionLayout>
   );
 };
