b75c5dfefc4799c9c6955f1be5669e3325eda203
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-05-18 12:00:25 +0300

    feature: list-page: added methods for class

--- src/components/list-page/LinkedList.ts
+++ src/components/list-page/LinkedList.ts
@@ -12,51 +12,45 @@ interface ILinkedList<T> {
   append: (element: T) => void;
   prepend: (element: T) => void;
   insertAt: (element: T, position: number) => void;
+  deleteTail: () => Node<T> | null;
+  deleteHead: () => Node<T> | null;
+  deleteByIndex: (index: number) => Node<T> | null;
+  getLastAddedNode: () => Node<T> | null;
+  findByIndex: (index: number) => Node<T> | null;
+  toArray: () => Node<T>[];
   getSize: () => number;
+  getLength: () => number;
 }
 
-class LinkedList<T> implements ILinkedList<T> {
+export class LinkedList<T> implements ILinkedList<T> {
   private head: Node<T> | null;
   private tail: Node<T> | null;
   private size: number;
   private length: number;
   private lastAddedNode: Node<T> | null;
 
-  constructor() {
+  constructor(values: T[] = []) {
     this.head = null;
     this.tail = null;
     this.size = 0;
     this.length = 0;
     this.lastAddedNode = null;
+    for (let val of values) {
+      this.append(val);
+    }
   }
 
-  insertAt(element: T, index: number) {
-    if (index < 0 || index > this.size) {
-      console.log('Enter a valid index');
-      return;
-    } else {
-      const node = new Node(element);
+  prepend(element: T) {
+    const node = new Node(element);
 
-      // добавить элемент в начало списка
-      if (index === 0) {
-        const temp = this.head;
-        this.head = node;
-        node.next = temp;
-      } else {
-        let curr = this.head;
-        let currIndex = 0;
-        while (curr && currIndex < index - 1) {
-          currIndex++;
-          curr = curr.next;
-        }
-        if (curr) {
-          node.next = curr.next;
-          curr.next = node;
-        }
-      }
+    this.head = node;
 
-      this.size++;
+    if (this.tail === null) {
+      this.tail = node;
     }
+    this.length++;
+    this.lastAddedNode = node;
+    return this;
   }
 
   append(element: T) {
@@ -74,22 +68,145 @@ class LinkedList<T> implements ILinkedList<T> {
     return this;
   }
 
-  prepend(element: T) {
-    const node = new Node(element);
-
-    this.head = node;
-
-    if (this.tail === null) {
-      this.tail = node;
-      this.length++;
+  deleteTail() {
+    if (!this.tail) {
+      return null;
     }
-    this.length++;
-    this.lastAddedNode = node;
-    return this;
+    const deletedTail = this.tail;
+    if (this.head === this.tail) {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return deletedTail;
+    }
+
+    let curr = this.head;
+    if (curr) {
+      while (curr.next) {
+
+        if (!curr.next.next) {
+          curr.next = null;
+        } else {
+          curr = curr.next;
+        }
+      }
+
+      this.tail = curr;
+      this.length--;
+      return deletedTail;
+    }
+    return null;
   }
+
+  deleteHead() {
+    if (!this.head) {
+      return null;
+    }
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+    this.length--;
+    return deletedHead;
+  }
+
+  deleteByIndex(index: number) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index out of bounds");
+    }
+
+    let deletedNode = null;
+
+    if (index === 0) {
+      return this.deleteHead();
+    }
+    if (index === this.length - 1) {
+      return this.deleteTail();
+    } else {
+      let prev = null;
+      let curr = this.head;
+      for (let i = 0; i < index; i++) {
+        if (curr) {
+          prev = curr;
+          curr = curr.next;
+        }
+      }
+      deletedNode = curr;
+      if (prev && curr) {
+        prev.next = curr.next;
+      }
+      this.length--;
+    }
+    return deletedNode;
+  }
+
+  findByIndex(index: number) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    }
+    let curr = this.head;
+    let i = 0;
+    while (i < index) {
+      if(curr) {
+        curr = curr.next;
+      }
+      i++;
+    }
+    return curr;
+  }
+
+  toArray() {
+    const nodes: Node<T>[] = [];
+    let curr = this.head;
+
+    while (curr) {
+      nodes.push(curr);
+      curr = curr.next;
+    }
+    return nodes;
+  }
+
+  insertAt(element: T, index: number) {
+    if (index < 0 || index > this.size) {
+      console.log('Enter a valid index');
+      return;
+    } else {
+      if (index === 0) {
+        this.prepend(element);
+      }
+      if (index === this.length - 1) {
+        this.append(element);
+      } else {
+        const node = new Node(element);
+        let curr = this.head;
+        let currIndex = 0;
+        while (curr && currIndex < index - 1) {
+          currIndex++;
+          curr = curr.next;
+        }
+        if (curr) {
+          node.next = curr.next;
+          curr.next = node;
+        }
+        this.length++;
+      }
+    }
+  }
+
 
   getSize() {
     return this.size;
   }
 
+  getLength() {
+    return this.length;
+  }
+
+  getLastAddedNode() {
+    return this.lastAddedNode;
+  }
 }
--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -4,10 +4,25 @@ import style from './list-page.module.css';
 import {Input} from "../ui/input/input";
 import {Button} from "../ui/button/button";
 import {Circle} from "../ui/circle/circle";
+import {TElement, TNumber} from "../../types/element";
+import {getRandomArr} from "../../utils/utils";
+import {LinkedList} from "./LinkedList";
+import {ElementStates} from "../../types/element-states";
 
 export const ListPage: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>("");
   const [inputIndex, setInputIndex] = useState<string | number>('');
+  const [linkedList] = useState(new LinkedList<TElement | TNumber>(getRandomArr(3,6).map((value: number) => {
+    return {value, color: ElementStates.Default};
+  })));
+  const [arrayToRender, setArrayToRender] = useState(linkedList.toArray());
+  const [loaderAddHead, setLoaderAddHead] = useState(false);
+  const [loaderAddTail, setLoaderAddTail] = useState(false);
+  const [loaderDelHead, setLoaderDelHead] = useState(false);
+  const [loaderDelTail, setLoaderDelTail] = useState(false);
+  const [loaderAddByIndex, setLoaderAddByIndex] = useState(false);
+  const [loaderDelByIndex, setLoaderDelByIndex] = useState(false);
+
   const handleInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   }
@@ -20,10 +35,10 @@ export const ListPage: React.FC = () => {
         <div className={style.container}>
           <Input type={"text"} maxLength={4} isLimitText={true} onChange={handleInputValue} value={inputValue}/>
           <div className={style.changeButtons}>
-            <Button linkedList="big" text={'Добавить в head'}/>
-            <Button linkedList="big" text={'Добавить в tail'}/>
-            <Button linkedList="big" text={'Удалить из head'}/>
-            <Button linkedList="big" text={'Удалить из tail'}/>
+            <Button isLoader={loaderAddHead} linkedList="big" text={'Добавить в head'}/>
+            <Button isLoader={loaderAddTail} linkedList="big" text={'Добавить в tail'}/>
+            <Button isLoader={loaderDelHead} linkedList="big" text={'Удалить из head'}/>
+            <Button isLoader={loaderDelTail} linkedList="big" text={'Удалить из tail'}/>
           </div>
         </div>
         <div className={style.container}>
@@ -31,8 +46,8 @@ export const ListPage: React.FC = () => {
           <Input type={"number"} onChange={handleInputIndex}
                  value={Number(inputIndex)}/>
           <div className={style.changeButtons}>
-            <Button linkedList="big" text={'Добавить по индексу'}/>
-            <Button linkedList="big" text={'Удалить по индексу'}/>
+            <Button isLoader={loaderAddByIndex} linkedList="big" text={'Добавить по индексу'}/>
+            <Button isLoader={loaderDelByIndex} linkedList="big" text={'Удалить по индексу'}/>
           </div>
         </div>
         <ul className={style.containerResult}>
