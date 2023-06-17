fb7926f3c09ff84fda9860147ff634a39a1592cd
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-06-17 16:30:41 +0300

    feature: added unit-test for buttons-props (linkedList and sorting)

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,7 +1,8 @@
-import { Button } from "./button";
-import { render, screen, fireEvent } from '@testing-library/react';
+import {Button} from "./button";
+import {fireEvent, render, screen} from '@testing-library/react';
 import renderer from "react-test-renderer";
 import React from 'react';
+import {Direction} from "../../../types/direction";
 
 describe('ButtonComponent', () => {
   it('Button with text', ()=> {
@@ -34,5 +35,29 @@ describe('ButtonComponent', () => {
     const button = screen.getByRole('button');
     fireEvent.click(button);
     expect(callback).toHaveBeenCalledTimes(1);
+  });
+  it('Button with sorting-Ascending', ()=> {
+    const button = renderer
+      .create(<Button sorting={Direction.Ascending}/>)
+      .toJSON();
+    expect(button).toMatchSnapshot();
+  });
+    it('Button with sorting-Descending', ()=> {
+      const button = renderer
+        .create(<Button sorting={Direction.Descending}/>)
+        .toJSON();
+      expect(button).toMatchSnapshot();
+  });
+  it('Button with linkedList-small', ()=> {
+    const button = renderer
+      .create(<Button linkedList={"small"}/>)
+      .toJSON();
+    expect(button).toMatchSnapshot();
+  });
+  it('Button with linkedList-big', ()=> {
+    const button = renderer
+      .create(<Button linkedList={"big"}/>)
+      .toJSON();
+    expect(button).toMatchSnapshot();
   });
 });
