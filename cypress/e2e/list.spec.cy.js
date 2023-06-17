08c73ee4f55c4126bc8127a52a05d7e1b0b1b656
Sergey Kukhtalev <s.kuhtalev@arktur.su>
2023-06-17 09:11:53 +0300

    feature: added e2e test in list.spec.cy.js for removed item from head

--- cypress/e2e/list.spec.cy.js
+++ cypress/e2e/list.spec.cy.js
@@ -63,4 +63,26 @@ describe('should work algorithm list', () => {
       cy.wrap(circle).should('have.css', 'border', DEFAULT_COLOR).invoke(text);
     });
   });
+  it('removed item in the head', () => {
+    cy.visit(BASE_URL + LIST);
+    cy.clock();
+    cy.get(CLEAR_BUTTON).first().click();
+    cy.tick(DELAY_IN_MS);
+    cy.tick(DELAY_IN_MS);
+    cy.get(CIRCLE).each((circle, index) => {
+      cy.wrap(circle)
+        .should('have.css', 'border', index === 1 ? CHANGING_COLOR : DEFAULT_COLOR)
+        .invoke('text')
+        .should(index === 0 ? 'be.empty' : 'not.be.empty');
+    });
+    cy.tick(DELAY_IN_MS);
+    cy.get(CIRCLE)
+      .each((circle) => {
+        cy.wrap(circle).should('have.css', 'border', DEFAULT_COLOR)
+          .should('not.be.empty')
+          .invoke('text');
+      });
+    cy.get(CIRCLE_HEAD).first().contains('head');
+    cy.get(CIRCLE_TAIL).last().contains('tail');
+  });
 });
